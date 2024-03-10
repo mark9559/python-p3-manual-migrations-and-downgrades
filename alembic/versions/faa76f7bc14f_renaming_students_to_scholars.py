c48f8e117ab43297e73ab0386b182c2056ed10d6
mark9559 <mwangimark57@gmail.com>
2024-03-10 23:23:39 +0300

    changes

---
+++ alembic/versions/faa76f7bc14f_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: faa76f7bc14f
+Revises: 
+Create Date: 2023-12-06 00:51:25.453647
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'faa76f7bc14f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
